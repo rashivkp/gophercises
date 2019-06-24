683aa2ab6504dd44520939e564e730c06792c208
Mohamed Rashid <rashivkp@gmail.com>
2019-06-24 12:48:43 +0530

    Complete Part 2 of Quizz

--- quizz/quizz.go
+++ quizz/quizz.go
@@ -1,57 +1,55 @@
 package main
 
-import ("fmt"
-		"os"
-		"encoding/csv"
-		"io"
-		"log"
-		"bufio"
-		"strings"
-		"flag"
-	)
+import (
+    "fmt"
+    "os"
+    "encoding/csv"
+    "log"
+    "bufio"
+    "strings"
+    "flag"
+    "time"
+)
+
+var right_answer, questions_count, seconds int = 0, 0, 0
+
+func countdown() {
+    for i := 0; i < seconds; i++ {
+        time.Sleep(1 * time.Second)
+    }
+    fmt.Println("\nScored:", right_answer, "out of ", questions_count)
+    os.Exit(0)
+}
 
 func main() {
-	fmt.Println("here starts the quizz")	
-	max_duration := flag.String("t", "", "Quiz max duration") 
+    fmt.Println("here starts the quizz")    
+    flag.IntVar(&seconds, "t", 30, "Quiz duration in seconds") 
 	flag.Parse()
-	if *max_duration == "" {
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
 
+    fmt.Println("Quizz Duration:", seconds, "\nPress Enter to start")
+    reader := bufio.NewReader(os.Stdin)
+    reader.ReadString('\n')
+    go countdown()
 
+    csv_file, _ := os.Open("problems.csv")
+    r := csv.NewReader(csv_file)
 
-	fmt.Println("Quizz Duration:", *max_duration, "\nPress Enter to start")
-	reader := bufio.NewReader(os.Stdin)
-	right_answer := 0
-	questions_count := 0
-	reader.ReadString('\n')
+    records, err := r.ReadAll()
+    if err != nil {
+        log.Fatal(err)
+    }
+    questions_count = len(records)
 
-	// qstn := "question,answer"
-	csv_file, _ := os.Open("problems.csv")
-	r := csv.NewReader(csv_file)
-
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		question := record[0]
-		answer := record[1]
-		fmt.Println(question)
-		user_answer, _ := reader.ReadString('\n')
-		user_answer = strings.Trim(user_answer, "\n")
-		fmt.Println(user_answer,)
-		if user_answer == answer {
-			right_answer = right_answer + 1
-		}
-		questions_count = questions_count + 1
-	}
-	score := right_answer*100/questions_count
-	fmt.Println("Total Questions", questions_count)
-	fmt.Println("Total Right Answers ", right_answer)
-	fmt.Println("Your score is", score)
+    for i:=0;i<questions_count;i++ {
+        question := records[i][0]
+        answer := records[i][1]
+        fmt.Println(question)
+        user_answer, _ := reader.ReadString('\n')
+        user_answer = strings.Trim(user_answer, "\n")
+        if user_answer == answer {
+            right_answer = right_answer + 1
+        }
+    }
+    fmt.Println("\nScored:", right_answer, "out of ", questions_count)
+    return
 }
